7e6582f1dbf6e215a84cd93c878db1d9e12caebc
Code <jordanov.k@outlook.com>
2022-12-06 18:13:20 +0100

    First Commit

---
+++ JavaScript/cas1/app.js
@@ -0,0 +1,14 @@
+var feetConversion = 0.3048;
+var feetLenght = prompt("Enter feet to convert in meters");
+var convert = feetLenght * feetConversion;
+console.log(`${feetLenght} stapki se ${convert} metri`);
+
+var widthSize = prompt("Enter the length of A side");
+var lengthSize = prompt("Enter the length of B side");
+var area = widthSize * lengthSize;
+console.log(`Plostinata na kvadratot e ${area}`);
+
+var p = 3.14;
+var radius = prompt("Vnesi radius");
+var area = p * (radius * radius);
+console.log(`The area of the circle is ${area}`);
---
+++ JavaScript/cas2/script.js
@@ -0,0 +1,6 @@
+console.log('It\`s really nice to be a programmer') // \ - strinG
+
+// undefined - nema vrednost promenlivata
+// null znaeme koj tip na promenliva e no nema vrednost deklarirana
+// NaN - matematicka operacija na dve promelivi od koj ednata ne e broj
+// 1/0 = Infinity - delenje so 0 e infinity
